700f237930e176912b6203631366bb4ca447da12
Alex <beccialex2002@gmail.com>
2023-07-05 13:41:22 -0300

    connect to mongoDb sucess

--- src/db.js
+++ src/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost/merndb');
+        await mongoose.connect(
+            'mongodb+srv://beccialex2002:Salamenopodes123@cluster0.zlsk9dg.mongodb.net/',
+            {
+                dbName:"Prueba"
+            });
         console.log("DB is connected")
 
     } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import app from "./app.js"
 import {connectDB} from './db.js'
 
-connectDB();
-app.listen(3000)
+await connectDB();
+app.listen(4000, () => {
+    console.log('Server on port' + 4000)
+})
 
-console.log('Server on port' + 3000)
