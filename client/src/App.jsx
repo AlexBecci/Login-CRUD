49a4d10c7f336ba511246367597b1bece67cf422
Alex <beccialex2002@gmail.com>
2023-07-06 15:40:09 -0300

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">HOLA MUNDO</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<h1 className="text-4xl font-bold">Home page</h1>}
+        />
+        <Route
+          path="/login"
+          element={<h1 className="text-4xl font-bold">login </h1>}
+        />
+        <Route
+          path="/register"
+          element={<h1 className="text-4xl font-bold">register </h1>}
+        />
+        <Route
+          path="/tasks"
+          element={<h1 className="text-4xl font-bold">Tasks</h1>}
+        />
+        <Route
+          path="/add-task"
+          element={<h1 className="text-4xl font-bold">New Taks </h1>}
+        />
+        <Route
+          path="/tasks/:id"
+          element={<h1 className="text-4xl font-bold">Update tasks</h1>}
+        />
+        <Route
+          path="/profile"
+          element={<h1 className="text-4xl font-bold">Profile</h1>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
