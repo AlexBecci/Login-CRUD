2e81532fb4acf6013fc2c97c6e30efde095281d4
Alex <beccialex2002@gmail.com>
2023-07-10 16:53:46 -0300

    TaskForm in process

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 function TaskFormPage() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
   return (
-    <div>TaskFormPage</div>
-  )
+    <div className="text-red-500">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          {...register("title")}
+          autoFocus
+        />
+
+        <textarea
+          rows="3"
+          placeholder="Description"
+          {...register("description")}
+        ></textarea>
+
+        <button>Save</button>
+      </form>
+    </div>
+  );
 }
 
-export default TaskFormPage
+export default TaskFormPage;
