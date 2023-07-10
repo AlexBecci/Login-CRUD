a3409405e7467a0079de204268f05afeb927f4f5
Alex <beccialex2002@gmail.com>
2023-07-10 20:35:44 -0300

    changed baseURL

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import tasksRoutes from "./routes/tasks.routes.js"
 const app = express()
 
 app.use(cors({
-    origin: 'https://login-crud.vercel.app/',
+    origin: 'http://localhost:5173',
     credentials: true
 }))
 app.use(morgan('dev'));
