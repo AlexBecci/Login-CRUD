2a0b34001b44a3261874b5355dc28d78fafa45ad
AlexBecci <beccialex2002@gmail.com>
2023-07-10 23:21:53 -0300

    padding

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { FiMail } from "react-icons/fi";
 function Footer() {
   return (
     <footer className="text-zinc-100 body-font">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-row justify-between">
+      <div className="container px-5 py-8 sm:py-2 mx-auto flex items-center sm:flex-row flex-row justify-between">
         
         <p className="text-sm text-gray-300  sm:mx-4 sm:px-4 sm:border-l-2 sm:border-r-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © 2023 —
