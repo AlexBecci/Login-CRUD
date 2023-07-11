48c96f9f80e6d62bc2ad2cf32c5bb7515cc59067
AlexBecci <beccialex2002@gmail.com>
2023-07-10 23:56:18 -0300

    login full responsive

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -11,7 +11,7 @@ function LoginPage() {
   } = useForm();
 
   const { singin, errors: singinErrors, isAuthenticated } = useAuth();
-  
+
   const navigate = useNavigate();
 
   const onSubmit = handleSubmit(async (data) => {
@@ -25,37 +25,58 @@ function LoginPage() {
   }, [isAuthenticated]);
 
   return (
-    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
-      {singinErrors.map((error, i) => (
-        <div className="bg-red-500 p-2 text-white" key={i}>
-          {error}
-        </div>
-      ))}
-      <h1>Login</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="email"
-          {...register("email", { required: true })}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md"
-        />
-        {errors.email && <p className="text-red-500">email is required</p>}
-        <input
-          type="password"
-          {...register("password", { required: true })}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md"
-        />
-        {errors.password && (
-          <p className="text-red-500">password is required</p>
-        )}
-        <button type="submit">Login</button>
-      </form>
-      <p className="flex gap-x-2 justify-between">
-        Don't have an account?{" "}
-        <Link className="text-cyan-500" to={"/register"}>
-          Sign up
-        </Link>
-      </p>
-    </div>
+    <>
+      <div className=" bg-gray-800 max-w-lg bg-opacity-50 rounded-lg p-8 flex flex-col md:mx-auto w-full my-32 md:my-8 ">
+        {singinErrors.map((error, i) => (
+          <div className="bg-red-500 p-2 text-white" key={i}>
+            {error}
+          </div>
+        ))}
+        <form onSubmit={onSubmit}>
+          <h2 className="text-white text-lg font-medium title-font mb-5">
+            Login
+          </h2>
+          <div className="relative mb-4">
+            <label for="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="email"
+              {...register("email", { required: true })}
+              className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-sky-900 rounded border border-gray-600 focus:border-sky-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+            {errors.username && (
+              <p className="text-red-500 my-1">email is required</p>
+            )}
+          </div>
+          <div className="relative mb-4">
+            <label for="email" className="leading-7 text-sm text-gray-400">
+              Password
+            </label>
+            <input
+              type="password"
+              {...register("password", { required: true })}
+              className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-sky-900 rounded border border-gray-600 focus:border-sky-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+            {errors.username && (
+              <p className="text-red-500 my-1">password is required</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-sky-500 border-0 py-2 px-8 focus:outline-none hover:bg-sky-600 rounded-xl text-lg mx-auto my-4"
+          >
+            Login
+          </button>
+        </form>
+        <p className="text-xs sm:text-lg my-4 mx-4 justify-between">
+          Don't have an account?
+          <Link className="text-sky-500" to={"/register"}>
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </>
   );
 }
 
